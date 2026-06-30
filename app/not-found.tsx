d8a945fb4b09f7cf5451f59b842398b8f06a1568
syalljaw <irsyalfaiz26@gmail.com>
2026-06-30 21:17:57 +0700

    Add app/not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-slate-900">
+      <div className="text-center text-white">
+        <h1 className="text-4xl font-black mb-4">404</h1>
+        <p className="text-slate-400">Halaman Tidak Ditemukan</p>
+      </div>
+    </div>
+  );
+}
